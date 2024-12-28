3b4233d2653391f7ee58c51f01fa69b9fa25065f
#EightSeven <hasheightseven@gmail.com>
2024-12-28 16:38:01 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-//import './index.css';
-import App from './App.js';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+//import "./index.css";
+import App from "./App.js";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('neon-monkey-popeye'));
+const root = ReactDOM.createRoot(document.getElementById("neon-monkey-bluestone"));
 root.render(
   <React.StrictMode>
     <App />
