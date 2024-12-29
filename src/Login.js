9b7b8f5ccca7e982117d696ef7d7dbe03273be30
#EightSeven <hasheightseven@gmail.com>
2024-12-29 18:05:49 +0530

    Update Login.js

--- src/Login.js
+++ src/Login.js
@@ -4,8 +4,8 @@ import "./App.css";
 function Login() {
     return (
         <form for="login">
-            <input name="username" type="text" maxLength=100 placeholder="username" required/>
-            <input name="password" type="password" maxLength=100 placeholder="password" required/>
+            <input name="username" type="text" maxLength="100" placeholder="username" required />
+            <input name="password" type="password" maxLength="100" placeholder="password" required />
             <button type="submit">Submit</button>
         </form>
     );
