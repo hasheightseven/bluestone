8258b6b253521811b6ff72e5637e158cd90391ca
#EightSeven <hasheightseven@gmail.com>
2024-12-29 17:56:51 +0530

    Create Login.js

---
+++ src/Login.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function Login() {
+    return (
+        <form for="login">
+            <input name="username" type="text" maxLength=100 placeholder="username" required/>
+            <input name="password" type="password" maxLength=100 placeholder="password" required/>
+            <button type="submit">Submit</button>
+        </form>
+    );
+}
