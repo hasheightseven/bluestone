c416f6e9036430408a1c31cb8ff5d5d2595d54fa
#EightSeven <hasheightseven@gmail.com>
2024-12-31 12:33:34 +0530

    changes

--- src/Login.js
+++ src/Login.js
@@ -3,10 +3,10 @@ import "./App.css";
 
 function Login() {
     return (
-        <form for="login">
-            <input name="username" type="text" maxLength="100" placeholder="username" required />
-            <input name="password" type="password" maxLength="100" placeholder="password" required />
-            <button type="submit">Submit</button>
+        <form for="login" id="nm-login">
+            <input name="username" id="nm-username" type="text" maxLength="100" placeholder="username" required />
+            <input name="password" id="nm-password" type="password" maxLength="100" placeholder="password" required />
+            <button id="nm-button" type="submit">Submit</button>
         </form>
     );
 }
