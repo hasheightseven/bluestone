aac895770b683a71fb6390e9cb0900c210ae8f49
#EightSeven <hasheightseven@gmail.com>
2025-01-23 14:26:05 +0530

    Create LandingBluestone.js

---
+++ src/components/LandingBluestone.js
@@ -0,0 +1,13 @@
+function LandingBluestone() {
+    return (
+        <div className="LandingBluestone">
+            <div className="LandingBluestoneTitle">welcome to bluestone buckinghum</div>
+            <div className="LandingBluestoneButtons">
+                <button type="button">login</button>
+                <button type="button">signup</button>
+            </div>
+        </div>
+    );
+}
+
+export default LandingBluestone;
