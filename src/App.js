e80fc224cd7a59dc56eaf625b3842758e7dca766
#EightSeven <hasheightseven@gmail.com>
2025-01-23 14:16:10 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import React from "react";
 import "./App.css";
-import Login from "./Login.js";
+import LandingBluestone from "components/LandingBluestone.js";
+
 
 function App(){
   return (
     <main className="App">
-      <div className=".AppDiv" id="App-Overlay">
-        <nav className=".AppNavbar">
-          <h1 className="AppH1">bluestone</h1>
-        </nav>
-        <div className="AppDiv" id="App-Login">
-          <Login />
-        </div>
-    </div>
+        <LandingBluestone />
     </main>
   );
 }
