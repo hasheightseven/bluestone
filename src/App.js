b8312d4442850420bc937d14043920413f4eb3e9
#EightSeven <hasheightseven@gmail.com>
2025-01-23 14:27:40 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import LandingBluestone from "components/LandingBluestone.js";
+import LandingBluestone from "./components/LandingBluestone.js";
 
 
 function App(){
