76a06c76a21f10c6e17d6b362d904096f30e21f9
#EightSeven <hasheightseven@gmail.com>
2025-01-24 22:44:46 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "./App.css";
 import LandingBluestone from "./components/LandingBluestone.js";
-
+import BottomMenu from "./components/BottomMenu.js";
 
 function App(){
   return (
     <div className="App">
         <LandingBluestone />
+        <BottomMenu />
     </div>
   );
 }
