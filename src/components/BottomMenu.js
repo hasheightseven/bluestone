460ca221d2e2ab553a78c918e8459baff9c83321
#EightSeven <hasheightseven@gmail.com>
2025-01-24 22:39:55 +0530

    Create BottomMenu.js

---
+++ src/components/BottomMenu.js
@@ -0,0 +1,23 @@
+function BottomMenu() {
+    return (
+        <div className="BottomMenu">
+            <div className="BomttomMenuChats">
+                <span className="BottomMenuIcon"></span>
+            </div>
+            <div className="BomttomMenuBrowse">
+                <span className="BottomMenuIcon"></span>
+            </div>
+            <div className="BomttomMenuCalls">
+                <span className="BottomMenuIcon"></span>
+            </div>
+            <div className="BomttomMenuStatus">
+                <span className="BottomMenuIcon"></span>
+            </div>
+            <div className="BomttomMenuProfile">
+                <span className="BottomMenuIcon"></span>
+            </div>
+        </div>
+    );
+}
+
+export default BottomMenu;
