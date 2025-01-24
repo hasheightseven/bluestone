53d99586f1c47ef86e6a877c74ce175f45637181
#EightSeven <hasheightseven@gmail.com>
2025-01-24 23:23:45 +0530

    Update BottomMenu.js

--- src/components/BottomMenu.js
+++ src/components/BottomMenu.js
@@ -22,7 +22,7 @@ function BottomMenu() {
 
 async function addeventlisteners(){
     const bmchats = document.querySelector(".BottomMenuChats");
-    bmchats.addEventListener("click", () => {
+    await bmchats.addEventListener("click", () => {
         //page to go to
         console.log("to-do chat list page");
     });
