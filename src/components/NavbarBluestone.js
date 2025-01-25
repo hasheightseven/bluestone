6cf2e562a1bf4d735b80c3b8b8984963e6022b9b
#EightSeven <hasheightseven@gmail.com>
2025-01-25 17:26:34 +0530

    Update NavbarBluestone.js

--- src/components/NavbarBluestone.js
+++ src/components/NavbarBluestone.js
@@ -3,7 +3,12 @@ function NavbarBluestone() {
     //const logolineone = document.createDlement("div");
     return (
         <nav>
-            
+            <div style="
+                width: 2.6vmin;
+                height: 26vmin;
+                margin: 8vmin 0 0 8vmin;
+                background-color: #abcdef;"
+                id="initial-div"></div>
             <div className="nav-center-space"></div>
             <div className="nav-burger-menu">
                 <div className="nav-bm-line1"></div>
