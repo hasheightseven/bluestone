df05f1459fced28a9c6d97efb6d344e2afef7225
#EightSeven <hasheightseven@gmail.com>
2025-01-25 14:46:18 +0530

    Create NavbarBluestone.js

---
+++ src/components/NavbarBluestone.js
@@ -0,0 +1,17 @@
+function NavbarBluestone() {
+    const logo = document.createElement("div");
+    const logolineone = document.createDlement("div");
+    return (
+        <nav>
+            ${{ logo }}
+            <div className="nav-center-space"></div>
+            <div className="nav-burger-menu">
+                <div className="nav-bm-line1"></div>
+                <div className="nav-bm-line2"></div>
+                <div className="nav-bm-line3"></div>
+            </div>
+        </nav>
+    );
+}
+
+export default NavbarBluestone;
