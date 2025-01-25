fabc41b873eac29118577db1857f26c675c25539
#EightSeven <hasheightseven@gmail.com>
2025-01-25 14:22:22 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
 import "./App.css";
+//import NavbarBluestone from "./components/NavbarBluestone.js";
 import LandingBluestone from "./components/LandingBluestone.js";
+//import ContactBluestone from "./components/ContactBluestone.js";
 import BottomMenu, { addeventlisteners } from "./components/BottomMenu.js";
 
 function App(){
   return (
     <div className="App">
-        <LandingBluestone />
-        <BottomMenu />
+        //<NavbarBluestone className="navbar-bluestone" />
+        <LandingBluestone className="landing-bluestone" />
+        //<ContactBluestone className="contact-bluestone" />
+        <BottomMenu className="bottom-menu" />
     </div>
   );
 }
