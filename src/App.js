a9b41002c0e19a2b5edc6bcbf81d3bea685336a8
#EightSeven <hasheightseven@gmail.com>
2025-01-25 14:50:46 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from "react";
 import "./App.css";
-//import NavbarBluestone from "./components/NavbarBluestone.js";
+import NavbarBluestone from "./components/NavbarBluestone.js";
 import LandingBluestone from "./components/LandingBluestone.js";
-//import ContactBluestone from "./components/ContactBluestone.js";
+import ContactBluestone from "./components/ContactBluestone.js";
 import BottomMenu, { addeventlisteners } from "./components/BottomMenu.js";
 
 function App(){
   return (
     <div className="App">
-        //<NavbarBluestone className="navbar-bluestone" />
+        <NavbarBluestone className="navbar-bluestone" />
         <LandingBluestone className="landing-bluestone" />
-        //<ContactBluestone className="contact-bluestone" />
+        <ContactBluestone className="contact-bluestone" />
         <BottomMenu className="bottom-menu" />
     </div>
   );
