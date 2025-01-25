80860a9f195fa6fbbbed225bf21b6af9ef63b275
#EightSeven <hasheightseven@gmail.com>
2025-01-25 14:50:23 +0530

    Create ContactBluestone.js

---
+++ src/components/ContactBluestone.js
@@ -0,0 +1,9 @@
+function ContactBluestone() {
+    return (
+        <div className="contact-bluestone">
+            <div className="request-to-register"></div>
+        </div>
+    );
+}
+
+export default ContactBluestone;
