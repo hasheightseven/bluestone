657ea2def5723cf8f7a9f31353dfc66f7b864653
#EightSeven <hasheightseven@gmail.com>
2025-01-25 17:01:30 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ function App(){
     <div className="App">
         <NavbarBluestone className="navbar-bluestone" />
         <LandingBluestone className="landing-bluestone" />
-        <CanvasBluestone />
         <ContactBluestone className="contact-bluestone" />
         <BottomMenu className="bottom-menu" />
     </div>
